8564637100a5764b3ed3be861fcd24bd5635bea9
AI-Ashik <arifulislamashik07@gmail.com>
2024-09-06 16:10:20 +0600

    initialized project

---
+++ index.js
@@ -0,0 +1,21 @@
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const path = require("path");
+
+const PORT = 3000;
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// server listen
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
