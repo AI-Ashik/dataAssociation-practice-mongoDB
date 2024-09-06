2d4fdeab755f61c48ed24b48edf0dcc746687825
AI-Ashik <arifulislamashik07@gmail.com>
2024-09-06 19:51:34 +0600

    data connect practice with hard coded value

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const path = require("path");
+const userModel = require("./models/user");
+const postModel = require("./models/post");
 
 const PORT = 3000;
 const app = express();
@@ -13,6 +15,29 @@ app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.render("index");
+});
+
+app.get("/create", async (req, res) => {
+  const user = await userModel.create({
+    username: "ashik",
+    email: "a@gmail.com",
+    age: 22,
+  });
+  res.send(user);
+});
+
+app.get("/create/post", async (req, res) => {
+  const post = await postModel.create({
+    postData: "blank",
+    user: "66db0717d81f4668bb0a999a",
+  });
+  const user = await userModel.findOne({ _id: "66db0717d81f4668bb0a999a" });
+  user.posts.push(user);
+  await user.save();
+  res.send({
+    user,
+    post,
+  });
 });
 
 // server listen
